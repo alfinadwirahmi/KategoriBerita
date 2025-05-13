6ea0dd0b22850ca022c23553b759300999cf75c2
Alfina Dwi Rahmi <123172082+alfinadwirahmi@users.noreply.github.com>
2025-05-13 22:35:08 +0700

    App.py

--- App.py
+++ App.py
@@ -21,7 +21,13 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 
 # Load spaCy model
-nlp = spacy.load("en_core_web_sm")
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except OSError:
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 # Preprocessing functions (same as training)
 def preserve_named_entities(text):
